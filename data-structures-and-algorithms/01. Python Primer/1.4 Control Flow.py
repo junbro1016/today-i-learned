5cd0a3abedc9225d195fa344192db3750de5922b
Junhyeong Park <pjh172839@gmail.com>
2024-03-10 20:32:19 +0900

    1.4 Control Flow

---
+++ data-structures-and-algorithms/01. Python Primer/1.4 Control Flow.py
@@ -0,0 +1,6 @@
+# 1.4 Control Flow 
+# 1.4.1 Conditionals
+# 1.4.2 Loops
+'''
+we can use a for loop in cases for which a while loop does not apply, such as when iterating through a collection, such as a set, that does not supply any direct form of indexing.
+'''
