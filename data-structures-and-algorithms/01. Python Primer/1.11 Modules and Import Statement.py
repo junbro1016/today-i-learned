773956c651d2d95254c7056baa89cd4c6806a40b
Junhyeong Park <pjh172839@gmail.com>
2024-03-12 15:29:12 +0900

    1.11 Modules and Import Statements

---
+++ data-structures-and-algorithms/01. Python Primer/1.11 Modules and Import Statement.py
@@ -0,0 +1,19 @@
+# 1.11 Modules and Import Statements
+# 1.11.1 Existing Modules
+'''
+array: provides compact array storage for primitive types
+collections: defines additional data structures and abstract base classes involving collections of objects
+copy: defines general functions for making copies of objects
+heapq: provides heap-based priority queue functions 
+math: defines common mathematical constants and functions
+os: provides support for interactions with the operating system
+random: provides random number generation
+re: provides support for processing regular expression
+sys: provides additonal level of interaction with the python interpreter
+time: provides support for measuring time, or delaying a program
+'''
+'''
+python's 'random' module provides support for pseudo-random number generation by defining a 'Random' class.
+instances of that class serve as generators with independent state.
+for convenience, all of the methods supoorted by the Random class are also supported as stand-alone functions of the random module.
+'''
